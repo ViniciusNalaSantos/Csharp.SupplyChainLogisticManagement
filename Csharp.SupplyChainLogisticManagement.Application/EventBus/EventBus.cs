96f3c07d02b866d673549897847d8cc14d33c8df
ViniciusNalaSantos <vinicius.nala@gmail.com>
2025-04-05 13:41:43 -0300

    Created interface for IBus class and its implementation.

---
+++ Csharp.SupplyChainLogisticManagement.Application/EventBus/EventBus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace Csharp.SupplyChainLogisticManagement.Application.EventBus;
+public class EventBus : IEventBus
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public EventBus(IPublishEndpoint publishEndpoint, ISendEndpointProvider sendEndpointProvider)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+    public Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : class
+    {
+        return _publishEndpoint.Publish(message, cancellationToken);
+    }
+
+    public Task SendAsync<T>(T command, CancellationToken cancellationToken = default) where T : class
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Csharp.SupplyChainLogisticManagement.Application/EventBus/IEventBus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.SupplyChainLogisticManagement.Application.EventBus;
+public interface IEventBus
+{
+    Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : class;
+    Task SendAsync<T>(T command, CancellationToken cancellationToken = default) where T : class;
+}
