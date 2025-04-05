645216e3f718c67ab22d1f6909579a0b17ca6bbc
ViniciusNalaSantos <vinicius.nala@gmail.com>
2025-04-05 14:16:56 -0300

    Created IMessageConsumer and its adapter.

---
+++ Csharp.SupplyChainLogisticManagement.Application/MessageConsumer/IMessageConsumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.SupplyChainLogisticManagement.Application.MessageConsumer;
+public interface IMessageConsumer<TMessage>
+{
+    Task ConsumeAsync(TMessage message, CancellationToken cancellationToken = default);
+}
---
+++ Csharp.SupplyChainLogisticManagement.Application/MessageConsumer/MassTransitMessageConsumerAdapter.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.SupplyChainLogisticManagement.Application.MessageConsumer;
+public class MassTransitConsumerAdapter<TMessage> : IConsumer<TMessage> where TMessage : class
+{
+    private readonly IMessageConsumer<TMessage> _inner;
+
+    public MassTransitConsumerAdapter(IMessageConsumer<TMessage> inner)
+    {
+        _inner = inner;
+    }
+
+    public Task Consume(ConsumeContext<TMessage> context)
+    {
+        return _inner.ConsumeAsync(context.Message);
+    }
+}
