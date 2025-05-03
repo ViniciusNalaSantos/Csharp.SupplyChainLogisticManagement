d02dff60aa35d7feeaecbbc301c7f43f5a5040db
ViniciusNalaSantos <vinicius.nala@gmail.com>
2025-05-03 11:54:56 -0300

    Created queries.

---
+++ Csharp.SupplyChainLogisticManagement.Application/Queries/GetOrderByEmissionDateQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.SupplyChainLogisticManagement.Application.Queries;
+public record GetOrderByEmissionDateQuery
+{
+    public DateTime EmissionDateStart { get; set; }
+    public DateTime EmissionDateEnd { get; set; }
+}
---
+++ Csharp.SupplyChainLogisticManagement.Application/Queries/GetOrderByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.SupplyChainLogisticManagement.Application.Queries;
+public record GetOrderByIdQuery
+{
+    public int Id { get; set; }
+}
