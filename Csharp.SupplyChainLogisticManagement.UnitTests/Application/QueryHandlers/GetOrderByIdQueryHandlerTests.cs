52e2ae81d228ce741ce904ed2c30e4877ab2485d
ViniciusNalaSantos <vinicius.nala@gmail.com>
2025-06-21 20:21:49 -0300

    Started developing unit tests.

---
+++ Csharp.SupplyChainLogisticManagement.UnitTests/Application/QueryHandlers/GetOrderByIdQueryHandlerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.SupplyChainLogisticManagement.UnitTests.Application.QueryHandlers;
+public sealed class GetOrderByIdQueryHandlerTests
+{
+    public void GivenGetOrderByIdQuery_ThenReturnCorrectOrder()
+    {
+        // Arrange
+
+        // Act
+        // Assert
+    }
+}
